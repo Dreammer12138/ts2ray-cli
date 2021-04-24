dec5d65362579d4d8935dbc8c7f8eea739343388
Dreammer12138 <wangshibo142@gmail.com>
2021-04-24 12:01:07 +0800

    cli

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # ts2ray-cli
 ts2ray cli tool
+
+## Install
+
+```shell
+$ sudo npm install -g ts2ray ts2ray-cli
+# or
+$ sudo yarn global add ts2ray ts2ray-cli
+```
+
+## How to use
+
+```shell
+$ ts2sub -h
+Usage: index [options]
+
+Options:
+  -v, --version      output the current version
+  -u, --url <url>    subscription url
+  -n, --name <name>  subscription name
+  -l, --list         list all subscription information
+  -p, --path <path>  output config in path (default: ".")
+  -h, --help         display help for command
+```
+
+**Example:**
+
+```shell
+$ ts2sub -u <url> -l
+* name ip
+* name ip
+* name ip
+```
+
+```shell
+$ ts2sub -u <url> -n <name> -p <config path>
+```
+
---
+++ index.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import { v2sub } from 'ts2ray';
+const colors = require('colors/safe');
+
+const program = new Command();
+program.version('0.0.1', '-v, --version', 'output the current version');
+
+program
+    .requiredOption('-u, --url <url>', 'subscription url')
+    .option('-n, --name <name>', 'subscription name')
+    .option('-l, --list', 'list all subscription information')
+    .option('-p, --path <path>', 'output config in path', '.')
+
+program.parse(process.argv);
+
+const options = program.opts();
+let v = new v2sub(options.url);
+
+if (options.list) {
+    for (let s in v.subs) {
+        console.log('* ' + colors.green(v.subs[s].ps), colors.cyan(v.subs[s].ip));
+    }
+}
+else v.toConfig(options.name, '.');
